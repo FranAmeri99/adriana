0a44801f026ac71c799935fe9f7e5ff451159d6e
FranAmeri99 <franameri2016@gmail.com>
2020-10-17 12:23:50 -0300

    iconos

--- AdrianaIconos/AdrianaApp/mainwindow.cpp
+++ AdrianaIconos/AdrianaApp/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFile>
+#include <QFileDevice>
+#include <QFileDialog>
+#include <QTextStream>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -15,7 +18,15 @@ MainWindow::~MainWindow()
 
 
 void MainWindow::on_actionGuardar_Como_triggered(){
-
+    QFile archivo;
+    QTextStream io;
+    QString nombre;
+    nombre = QFileDialog::getSaveFileName(this, "Guardar");
+    archivo.setFileName(nombre);
+    archivo.open(QIODevice::WriteOnly | QIODevice::Text);
+    io.setDevice(&archivo);
+    io << "el pepe";
+    archivo.close();
 }
 
 void MainWindow::on_actionNuevo_triggered(){
